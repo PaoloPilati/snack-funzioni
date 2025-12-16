e57160477be190201ed5f27f9816a0e08c90a976
Zizou10 <paolo.pilati93@gmail.com>
2025-12-16 10:55:10 +0100

    02-Iniziali: funzione map standard + funzione arrow

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstLetter(array) {
+    return array.map (name => name[0]);
+}
+
+//Arrow function
+
+//const firstLetter = array => array.map (name => name[0]);
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetter(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
