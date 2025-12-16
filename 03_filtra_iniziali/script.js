9a642077546e568f6b410e3418decb0b1df03a4f
Zizou10 <paolo.pilati93@gmail.com>
2025-12-16 11:39:14 +0100

    03-Filtra iniziali: funzione filter standard + funzione esplosa + arrow function. L'aggiunta di .toUpperCase permette di selezionare la prima lettera anche scrivendo la minuscola

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,25 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+
+function pickByFirstLetter (array, initial) {
+        return array.filter(name => name[0].toUpperCase() === initial.toUpperCase());
+}
+
+//funzione esplosa:
+    //return array.filter(function (name) {
+        //return name[0] === initial;}
+        //);
+
+//Arrow function:
+
+//const pickByFirstLetter = (array, initial) => array.filter(name => name[0] === initial);
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(pickByFirstLetter(names, "a"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
