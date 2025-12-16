a40bc73560959cde4c87b794460c72c90627c099
Zizou10 <paolo.pilati93@gmail.com>
2025-12-16 18:38:36 +0100

    05-Bonus: funzione con let hour = new  Date().getHours()

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,22 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+//*FONTI: https://medium.com/@elechristacoal/the-new-date-gethours-method-in-javascript-24cee15f1468
+
+function greetingsFormula (username) {
+//ARROW FUNCTION: const greetingsFormula = (username) => {...}
+    let hour = new Date().getHours();
+
+    if (hour < 13) {
+        return `Buongiorno, ${username}.`;
+    } else if (hour < 17) {
+        return `Buon pomeriggio, ${username}.`;
+    } else {
+        return `Buonasera, ${username}.`;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(greetingsFormula(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
