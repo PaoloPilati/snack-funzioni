6a9544d33a6bab980b1510b624073ed21bc792d1
Zizou10 <paolo.pilati93@gmail.com>
2025-12-16 14:57:40 +0100

    04-ContaVocali: funzione con ciclo for + arrow function con ciclo for per contare le vocali. TODO: mostrare le vocali presenti nella variabile word

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 // Dichiara la funzione qui.
+function charCounter (string, character) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (character.includes(string[i].toLowerCase())) {
+            count++;
+            }
+}
+return count;
+}
 
+//inizio arrow function (poi continua con lo stesso ciclo for): const charCounter = (string, character) => {...
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(charCounter(word, vowels));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
